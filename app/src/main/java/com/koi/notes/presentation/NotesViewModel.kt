4088812a3acfb421016067a6f6b847e7927863a6
khairlikhwn <khairulmn83@gmail.com>
2024-04-26 18:13:01 +0800

    add note view model

---
+++ app/src/main/java/com/koi/notes/presentation/NotesViewModel.kt
@@ -0,0 +1,11 @@
+package com.koi.notes.presentation
+
+import androidx.lifecycle.ViewModel
+import com.koi.notes.data.NoteDao
+
+class NotesViewModel(
+    private val dao: NoteDao
+): ViewModel() {
+
+
+}
