badba7502e02089235a5331ed3a6e9dfe9e25080
khairlikhwn <khairulmn83@gmail.com>
2024-04-26 18:01:25 +0800

    add note database

---
+++ app/src/main/java/com/koi/notes/data/NotesDatabase.kt
@@ -0,0 +1,13 @@
+package com.koi.notes.data
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+
+@Database(
+    entities = [Note::class],
+    version = 1
+)
+abstract class NotesDatabase: RoomDatabase() {
+    abstract val dao: NoteDao
+}
