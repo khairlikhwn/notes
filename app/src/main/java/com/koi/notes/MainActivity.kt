b0b466fab8c0ef8b0c0ecf8d337ea36b46ab1b86
khairlikhwn <khairulmn83@gmail.com>
2024-04-28 10:42:41 +0800

    update main activity

--- app/src/main/java/com/koi/notes/MainActivity.kt
+++ app/src/main/java/com/koi/notes/MainActivity.kt
@@ -3,6 +3,7 @@ package com.koi.notes
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -10,9 +11,33 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.room.Room
+import com.koi.notes.data.NotesDatabase
+import com.koi.notes.presentation.NotesViewModel
 import com.koi.notes.ui.theme.NotesTheme
 
 class MainActivity : ComponentActivity() {
+
+    private val database by lazy {
+        Room.databaseBuilder(
+            applicationContext,
+            NotesDatabase::class.java,
+            "notes.db"
+        ).build()
+    }
+
+    private val viewModel by viewModels<NotesViewModel> (
+        factoryProducer = {
+            object : ViewModelProvider.Factory {
+                override fun<T: ViewModel> create(modelClass: Class<T>): T {
+                    return NotesViewModel(database.dao) as T
+                }
+            }
+        }
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
